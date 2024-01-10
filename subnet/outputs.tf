ee1d07e9f77439c7461075d94d815d3e8d54167c
Arek <arobrooklyn@gmail.com>
2024-01-09 22:07:38 -0500

    cleaned up outputs

--- subnet/outputs.tf
+++ subnet/outputs.tf
@@ -1,21 +1,3 @@
-/*
-output "public_subnet_id" {
-  value = aws_subnet.each.public_subnet.id
-}
-
-output "public_subnet_cidr" {
-  value = aws_subnet.each.public_subnet.cidr_block
-}
-
-output "private_subnet_id" {
-  value = aws_subnet.each.private_subnet.id
-}
-
-output "private_subnet_cidr" {
-  value = aws_subnet.each.private_subnet.cidr_block
-}
-*/
-
 output "subnet_ids" {
   value = {
     for k, v in aws_subnet.subnet : k => v.id
